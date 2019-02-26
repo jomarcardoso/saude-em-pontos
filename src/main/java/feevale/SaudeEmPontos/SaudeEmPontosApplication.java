daa48f1aff03b433aa8280203c5a794eeca964fd
Jomar Antonio Cardoso <jomar.cardoso@cwi.com.br>
2019-02-26 18:24:45 -0300

    initial files

---
+++ src/main/java/feevale/SaudeEmPontos/SaudeEmPontosApplication.java
@@ -0,0 +1,13 @@
+package feevale.SaudeEmPontos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaudeEmPontosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaudeEmPontosApplication.class, args);
+	}
+
+}
