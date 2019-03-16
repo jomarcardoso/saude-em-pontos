ba79bf5664572af91207a9464493b7d42efa2a9b
jomar.cardoso <Jo5592720>
2019-03-16 14:31:04 -0300

    feat alimento controller

---
+++ src/main/java/feevale/saude/controllers/AlimentoController.java
@@ -0,0 +1,60 @@
+package feevale.saude.controllers;
+
+import feevale.saude.dto.AlimentoDTO;
+import feevale.saude.entity.Alimento;
+import feevale.saude.service.AlimentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/alimento")
+public class AlimentoController {
+
+    @Autowired
+    private AlimentoService alimentoService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<AlimentoDTO>> find() {
+        List<Alimento> alimentoList = alimentoService.findAll();
+        List<AlimentoDTO> categoryDTOS = alimentoList.stream().map(cl -> new AlimentoDTO(cl)).collect(Collectors.toList());
+
+        return ResponseEntity.ok().body(categoryDTOS);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Alimento> find(@PathVariable final Integer id) {
+        Alimento category = alimentoService.find(id);
+
+        return ResponseEntity.ok().body(category);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody Alimento category) {
+        category = alimentoService.insert(category);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(category.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Void> update(@RequestBody Alimento alimento, @PathVariable Integer id) {
+        alimento.setId(id);
+        alimentoService.update(alimento);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable final Integer id) {
+        alimentoService.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
+}
