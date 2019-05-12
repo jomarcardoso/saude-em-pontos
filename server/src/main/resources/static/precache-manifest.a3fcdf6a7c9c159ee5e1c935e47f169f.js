703a2cc8411b8dcaca97dcd8787ccd298702e999
jomar.cardoso <Jo5592720>
2019-05-12 11:07:47 -0300

    feat(server): statics

---
+++ server/src/main/resources/static/precache-manifest.a3fcdf6a7c9c159ee5e1c935e47f169f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7f7da98a838284c8bd958e4d9d1ac34",
+    "url": "/static/media/tipo-atividade-fisica.e7f7da98.svg"
+  },
+  {
+    "revision": "95f14d60af17b4bcad885d009bfa9a5a",
+    "url": "/static/media/refeicao.95f14d60.svg"
+  },
+  {
+    "revision": "ed6accd82e0d0571ba9efa5352bb3923",
+    "url": "/static/media/exercicio.ed6accd8.svg"
+  },
+  {
+    "revision": "71029fa778f43073dbae59183203cb74",
+    "url": "/static/media/atividade-fisica.71029fa7.svg"
+  },
+  {
+    "revision": "08e65fc1110876db172b49446c3b9d00",
+    "url": "/static/media/alimento.08e65fc1.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "882b6300568577c747ec",
+    "url": "/static/js/main.446e484f.chunk.js"
+  },
+  {
+    "revision": "63289c1d7f92015e65f7",
+    "url": "/static/js/2.89756c05.chunk.js"
+  },
+  {
+    "revision": "882b6300568577c747ec",
+    "url": "/static/css/main.bbb6b904.chunk.css"
+  },
+  {
+    "revision": "8493ec0f7cf4da3445df3e6477dbb6fd",
+    "url": "/index.html"
+  }
+];
