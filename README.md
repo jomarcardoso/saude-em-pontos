c559c60e97658a15ce2f31b1301b3a802b66b653
jomar.cardoso <Jo5592720>
2019-06-02 14:55:51 -0300

    relatorio dica

---
+++ README.md
@@ -0,0 +1,1 @@
+[tabela de indice glicemico](http://www.cookie.com.br/site/wp-content/uploads/2014/11/Tabela-Internacional-de-%C3%8Dndice-Glic%C3%AAmico-IG-e-Carga-Glic%C3%AAmica-CG-%E2%80%93-Revisada.pdf)
--- client/src/domain/relatorio/Relatorio.js
+++ client/src/domain/relatorio/Relatorio.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import WithDomain from '../../components/with-domain/WithDomain';
+import './relatorio.scss';
 
 function Relatorio({ send, form, handleEdit, handleDelete, table }) {
   console.log(table);
@@ -8,42 +9,45 @@ function Relatorio({ send, form, handleEdit, handleDelete, table }) {
       <h3>Relatório</h3>
       <div className="row">
         <div className="col">
-          <div class="card">
-            <div class="card-header">
+          <div className="card">
+            <div className="card-header">
               Total calorias
             </div>
-            <div class="card-body">
-              <h5 class="card-title">{table.calorias}</h5>
-              {/* <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a> */}
+            <div className="card-body">
+              <h2 className="card-title text-secondary">{table.calorias}</h2>
+              {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" className="btn btn-primary">Go somewhere</a> */}
             </div>
           </div>
         </div>
         <div className="col">
-          <div class="card">
-            <div class="card-header">
+          <div className="card">
+            <div className="card-header">
               Total inflamatório
             </div>
-            <div class="card-body">
-              <h5 class="card-title">{table.inflamatorio}</h5>
-              {/* <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a> */}
+            <div className="card-body">
+              <h2 className="card-title text-secondary">{table.inflamatorio}</h2>
+              {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" className="btn btn-primary">Go somewhere</a> */}
             </div>
           </div>
         </div>
         <div className="col">
-          <div class="card">
-            <div class="card-header">
+          <div className="card">
+            <div className="card-header">
               Total acidificação
             </div>
-            <div class="card-body">
-              <h5 class="card-title">{table.acidificacao}</h5>
-              {/* <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a> */}
+            <div className="card-body">
+              <h2 className="card-title text-secondary">{table.acidificacao}</h2>
+              {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" className="btn btn-primary">Go somewhere</a> */}
             </div>
           </div>
         </div>
       </div>
+      <p className="Relatorio-dica lead text-center px-3 py-3 mx-auto my-3">
+        {table.dica || 'situação estranha, não tenho o que dizer'}
+      </p>
     </>
   )
 }
